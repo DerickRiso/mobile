36d3c549b2628987dee469a16d350bf858f139b9
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2024-12-11 13:35:26 -0300

    Componentes de boas vindas criados

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,13 @@
 import { View, Text } from "react-native"
 
+import { Welcome } from "@/components/welcome" 
+
 export default function Index(){
     return (
         // Dentro de um return apenas um elemento pai possui retorno (view)
         <View
-        style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-        }}
-        >
-            <Text style={{ fontSize: 22 }}>Hello React Native</Text>
+        style={{flex: 1, padding: 40, gap: 40}}>
+            <Welcome />
         </View>
     )
 }
