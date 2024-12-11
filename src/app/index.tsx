a3ead38595fa6d859d06666fb400d979cb4baa7a
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2024-12-11 14:38:05 -0300

    Página de boas vindas concluída

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text } from "react-native"
 
 import { Welcome } from "@/components/welcome" 
+import { Steps } from "@/components/steps"
 
 export default function Index(){
     return (
@@ -8,6 +9,7 @@ export default function Index(){
         <View
         style={{flex: 1, padding: 40, gap: 40}}>
             <Welcome />
+            <Steps />
         </View>
     )
 }
