e700d895c85f6c2d688267fb38f3d3df1b8b4c3c
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2024-12-12 11:36:42 -0300

    Botão feito

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,20 @@
 import { View, Text } from "react-native"
 
 import { Welcome } from "@/components/welcome" 
+import { Button } from "@/components/button"
 import { Steps } from "@/components/steps"
 
 export default function Index(){
     return (
-        // Dentro de um return apenas um elemento pai possui retorno (view)
         <View
         style={{flex: 1, padding: 40, gap: 40}}>
             <Welcome />
             <Steps />
+            
+            <Button isLoading={false}>
+                <Button.Title>Começar</Button.Title>
+                
+            </Button>
         </View>
     )
 }
